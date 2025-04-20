e327b601e4e998cd2e1433ee21d9a2cdd866f168
Linh0508 <161113539+Linh0508@users.noreply.github.com>
2025-04-20 12:25:37 +0700

    Update Program.cs

--- Lab01/Lab01_2/Program.cs
+++ Lab01/Lab01_2/Program.cs
@@ -1,11 +1,11 @@
 ﻿using System.Text;
 
 Console.OutputEncoding = Encoding.UTF8;
-
-double dai, rong;
+double dai;
+double rong;
 Console.Write("Nhập chiều dài: ");
 dai = double.Parse(Console.ReadLine());
 Console.Write("Nhập chiều rộng: ");
 rong = double.Parse(Console.ReadLine());
 double dienTich = dai * rong;
-Console.WriteLine($"Diện tích hình chữ nhật là: {dienTich}");
+Console.WriteLine($"Diện tích là: {dienTich}");
