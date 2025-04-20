9d96d01f3a03e241ae62ce9f26a181ed852eda92
Linh0508 <161113539+Linh0508@users.noreply.github.com>
2025-04-20 12:24:51 +0700

    Update Program.cs

--- Lab01/Lab01_1/Program.cs
+++ Lab01/Lab01_1/Program.cs
@@ -1,11 +1,10 @@
 ﻿using System.Text;
 
 Console.OutputEncoding = Encoding.UTF8;
-
 string ten;
 int tuoi;
-Console.Write("Nhập tên của bạn: ");
+Console.Write("Nhập tên: ");
 ten = Console.ReadLine();
-Console.Write("Nhập tuổi của bạn: ");
+Console.Write("Nhập tuổi: ");
 tuoi = int.Parse(Console.ReadLine());
 Console.WriteLine($"Xin chào {ten}, bạn {tuoi} tuổi!");
