d6657fd99810e6f996b5cf2d057cddc9cd05e148
Linh0508 <161113539+Linh0508@users.noreply.github.com>
2025-04-27 18:20:00 +0700

    Add files via upload

---
+++ Lab_Tuan3/Lab02/Controllers/TodoController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lab02.Controllers
+{
+    public class TodoController : Controller
+    {
+        [Route("/todoHome")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Route("/todoAdd")]
+        public IActionResult Add()
+        {
+            return View();
+        }
+        [Route("/todoEdit")]
+        public IActionResult Edit()
+        {
+            return View();
+        }
+    }
+}
