b46bc58557d56fff3c0c6f18efe84688d3fe49fc
Linh0508 <161113539+Linh0508@users.noreply.github.com>
2025-05-04 11:58:05 +0700

    Upload tuan 4

---
+++ Lab_Tuan4/Lab/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using Lab.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab.Controllers
+{
+    public class ProductController : Controller
+    {
+        static List<Product> products = new List<Product>() 
+        {
+            new Product() { Id = 1, Name = "Bim bim", Price = 10000 },
+            new Product() { Id = 2, Name = "Nước ngọt", Price = 15000 },
+            new Product() { Id = 3, Name = "Bánh", Price = 30000 }
+        };
+        [Route("/product-details")]
+        public IActionResult Details()
+        {
+            return View(products);
+        }
+    }
+}
